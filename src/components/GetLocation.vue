12587e5dc659b6f0df5c58fe02d15fa3cb436599
capgemini-salvgonz <chava.gnolasco@gmail.com>
2025-06-24 20:56:16 -0600

    Initial commit

---
+++ src/components/GetLocation.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+type Geolocation = {
+  latitude: number;
+  longitude: number;
+};
+
+const coords: Ref<Geolocation | undefined> = ref();
+
+</script>
